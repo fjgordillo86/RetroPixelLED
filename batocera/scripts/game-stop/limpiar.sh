6901c8bafbf4b1be4f7c0b2a9a74bb92ac722a1e
fjgordillo86 <59791834+fjgordillo86@users.noreply.github.com>
2026-01-11 17:07:27 +0100

    Delete batocera/scripts/game-stop directory

--- batocera/scripts/game-stop/limpiar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Configuración: Pon aquí la IP del ESP32
-IP_ESP32="192.168.xxx.xxx"
-
-# Enviamos el comando "DEFAULT" al salir del juego
-curl -s "http://$IP_ESP32/game?name=DEFAULT" &
